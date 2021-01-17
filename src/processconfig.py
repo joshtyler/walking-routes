0da452a2eb71fbcaa727827a398e14293103a90c
Joshua Tyler <josh.tyler@btinternet.com>
2021-01-17 23:52:19 +0000

    Start plumbing into top level

--- src/processconfig.py
+++ src/processconfig.py
@@ -5,7 +5,9 @@ import toml
 from pathlib import Path
 import sys
 
-import processgpsbabelkml as kml
+import kml
+import gpsbabel_kml
+import os_trigs
 
 def sanitise_section_args(args_dict):
     # If a description was provided use it, otherwise don't have this element
@@ -16,18 +18,18 @@ def sanitise_section_args(args_dict):
         args_dict["dates"] = list()
     return args_dict
 
-parser = argparse.ArgumentParser(description="Process TOML files of walks")
+parser = argparse.ArgumentParser(description="Process TOML files of walks into one KML")
 parser.add_argument('files', metavar='F', type=str, nargs='+', help='TOML file(s) to process')
 args = parser.parse_args()
 
-output_root = kml.create_top_level_kml()
+output_root = kml.create_root()
 
 for file in args.files:
     t = toml.load(file)
 
     # Create a folder for the contents of this toml file
     # Use the filename for the KML folder (but with underscores replaced with spaces)
-    folder = kml.add_folder_to_kml(output_root, Path(file).stem.replace("_", " "))
+    folder = kml.add_folder(output_root, Path(file).stem.replace("_", " "))
 
     # Process all walks in file
     for name, args in t.items():
@@ -39,10 +41,10 @@ for file in args.files:
             filename = "../gen/kml/"+name.replace(" ", "_")+".kml"
 
             # Add in the walk
-            kml.process_kml_file(folder, filename, name, args["description"])
+            gpsbabel_kml.append_gpsbabel_kml_paths_to_folder(folder, filename, name, args["description"])
         elif t["filetype"] == "trig":
-            // DO THINGS
-            assert False
+            trigs = os_trigs.process_csv(t["datasource"])
+           # Filter out based on distance and add to tree with metadata here
         else:
             assert False, "Unknown file type %s" %(t["filetype"])
 
